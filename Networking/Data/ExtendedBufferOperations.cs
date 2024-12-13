f13061283ceea14ed66ea27b89c48c9a0a0ed7e2
Jeremy Glazebrook <xxjdrocks450xx@hotmail.com>
2024-12-13 02:04:29 -0500

    Add BroadcastDataBlobPDU Refactor TSOSerializer framework

---
+++ Networking/Data/ExtendedBufferOperations.cs
@@ -0,0 +1,83 @@
+﻿using MiscUtil.Conversion;
+using System;
+using System.IO;
+
+namespace QuazarAPI.Networking.Data
+{
+    public abstract class ExtendedBufferOperations : IDisposable
+    {
+        public virtual uint BodyLength => (uint)_bodyBuffer.Length;
+        protected MemoryStream _bodyBuffer = new MemoryStream();
+        private bool disposedValue;
+        public byte[] Body
+        {
+            get => _bodyBuffer.ToArray();
+            set
+            {
+                AllocateBody((uint)value.Length);
+                EmplaceBody(value);
+            }
+        }
+        public long BodyPosition => _bodyBuffer.Position;
+        public bool IsBodyEOF => _bodyBuffer.Position == BodyLength;
+
+        public void AllocateBody(uint BodySize) => _bodyBuffer.AllocateBody(BodySize);
+        public void ReallocateBody(uint NewSize) => _bodyBuffer.ReallocateBody(NewSize);
+        public void SetPosition(int Position) => _bodyBuffer.Position = Position;
+        public void EmplaceBody(params byte[] Bytes) => _bodyBuffer.EmplaceBody(Bytes);
+        public void EmplaceBody(uint DWORD, Endianness Endian = Endianness.BigEndian) => _bodyBuffer.EmplaceBody(DWORD, Endian);
+        public void EmplaceBodyAt(int Position, byte[] Buffer) => _bodyBuffer.EmplaceBodyAt(Position, Buffer);
+        public void EmplaceBodyAt(int Position, uint DWORD, Endianness Endian = Endianness.BigEndian) => _bodyBuffer.EmplaceBodyAt(Position, DWORD, Endian);
+        public void EmplaceBodyAt(int Position, byte Byte) => _bodyBuffer.EmplaceBodyAt(Position, Byte);
+
+        /// <summary>
+        /// The byte, cast to an int
+        /// </summary>
+        /// <returns></returns>
+        public int ReadBodyByte() => _bodyBuffer.ReadBodyByte();
+        public byte[] ReadBodyByteArray(int Length) => _bodyBuffer.ReadBodyByteArray(Length);
+        public ushort ReadBodyUshort(Endianness Endian = Endianness.BigEndian) => _bodyBuffer.ReadBodyUshort(Endian);
+        public uint ReadBodyDword(Endianness Endian = Endianness.BigEndian) => _bodyBuffer.ReadBodyDword(Endian);
+        public string ReadBodyNullTerminatedString(int MaxSize) => _bodyBuffer.ReadBodyNullTerminatedString(MaxSize);
+        public string ReadBodyNullTerminatedString(int Offset, int MaxSize) => _bodyBuffer.ReadBodyNullTerminatedString(Offset, MaxSize);
+        
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    _bodyBuffer.Dispose();
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+                // TODO: set large fields to null
+                disposedValue = true;
+            }
+        }
+        /// <summary>
+        /// Advances the <see cref="BodyPosition"/> by the <paramref name="amount"/> given.
+        /// </summary>
+        /// <param name="amount"></param>
+        public void Advance(int amount = 1) => SetPosition((int)BodyPosition + amount);
+        /// <summary>
+        /// Reads all data until the end of the buffer
+        /// </summary>
+        /// <exception cref="NotImplementedException"></exception>
+        public byte[] ReadToEnd() => ReadBodyByteArray((int)(BodyLength - BodyPosition));
+
+        // // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+        // ~TPWPacket()
+        // {
+        //     // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+        //     Dispose(disposing: false);
+        // }
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ Networking/Data/ExtendedBufferOperationsExtensions.cs
@@ -0,0 +1,112 @@
+﻿using MiscUtil.Conversion;
+using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace QuazarAPI.Networking.Data
+{
+    public static class ExtendedBufferOperationsExtensions
+    {
+        public static void AllocateBody(this Stream _bodyBuffer, uint BodySize)
+        {            
+            _bodyBuffer.SetLength(BodySize);
+            _bodyBuffer.Write(new byte[BodySize], 0, (int)BodySize);
+            SetPosition(_bodyBuffer, 0);
+        }
+        public static void ReallocateBody(this Stream _bodyBuffer, uint NewSize)
+        {
+            _bodyBuffer.SetLength(NewSize);
+        }
+        public static void SetPosition(this Stream _bodyBuffer, int Position) => _bodyBuffer.Position = Position;
+        public static void EmplaceBody(this Stream _bodyBuffer, params byte[] Bytes)
+        {
+            if ((Bytes.Length + _bodyBuffer.Position) > _bodyBuffer.Length)
+                ReallocateBody(_bodyBuffer, (uint)(Bytes.Length + _bodyBuffer.Position));
+            _bodyBuffer.Write(Bytes, 0, Bytes.Length);
+        }
+        public static void EmplaceBody(this Stream _bodyBuffer, uint DWORD, Endianness Endian = Endianness.BigEndian)
+        {
+            if (Endian == Endianness.BigEndian)
+                EmplaceBody(_bodyBuffer, EndianBitConverter.Big.GetBytes(DWORD));
+            else EmplaceBody(_bodyBuffer, EndianBitConverter.Little.GetBytes(DWORD));
+        }
+        public static void EmplaceBodyAt(this Stream _bodyBuffer, int Position, byte[] Buffer)
+        {
+            SetPosition(_bodyBuffer, Position);
+            EmplaceBody(_bodyBuffer, Buffer);
+        }
+        public static void EmplaceBodyAt(this Stream _bodyBuffer, int Position, uint DWORD, Endianness Endian = Endianness.BigEndian)
+        {
+            SetPosition(_bodyBuffer, Position);
+            EmplaceBody(_bodyBuffer, DWORD, Endian);
+        }
+        public static void EmplaceBodyAt(this Stream _bodyBuffer, int Position, byte Byte)
+        {
+            SetPosition(_bodyBuffer, Position);
+            EmplaceBody(_bodyBuffer, Byte);
+        }
+
+        /// <summary>
+        /// The byte, cast to an int
+        /// </summary>
+        /// <returns></returns>
+        public static int ReadBodyByte(this Stream _bodyBuffer)
+        {
+            return (byte)_bodyBuffer.ReadByte();
+        }
+        public static byte[] ReadBodyByteArray(this Stream _bodyBuffer, int Length)
+        {
+            byte[] array = new byte[Length];
+            _bodyBuffer.Read(array, 0, Length);
+            return array;
+        }
+        public static ushort ReadBodyUshort(this Stream _bodyBuffer, Endianness Endian = Endianness.BigEndian)
+        {
+            if (Endian == Endianness.BigEndian)
+                return EndianBitConverter.Big.ToUInt16(ReadBodyByteArray(_bodyBuffer, 2), 0);
+            else return EndianBitConverter.Little.ToUInt16(ReadBodyByteArray(_bodyBuffer, 2), 0);
+        }
+        public static uint ReadBodyDword(this Stream _bodyBuffer, Endianness Endian = Endianness.BigEndian)
+        {
+            if (Endian == Endianness.BigEndian)
+                return EndianBitConverter.Big.ToUInt32(ReadBodyByteArray(_bodyBuffer, 4), 0);
+            else return EndianBitConverter.Little.ToUInt32(ReadBodyByteArray(_bodyBuffer, 4), 0);
+        }
+        public static string ReadBodyNullTerminatedString(this Stream _bodyBuffer, int MaxSize)
+        {
+            long BodyLength = _bodyBuffer.Length;
+            long BodyPosition = _bodyBuffer.Position;
+            int readLen = Math.Min(MaxSize, (int)(BodyLength - BodyPosition));
+            byte[] str = ReadBodyByteArray(_bodyBuffer, readLen);
+            string myStr = "";
+            myStr = Encoding.UTF8.GetString(str);
+            if (myStr.Contains('\0'))
+                myStr = myStr.Remove(myStr.IndexOf('\0'));
+            return myStr;
+            for (int i = 0; i < readLen; i += 2)
+            {
+                short myChar = 0;
+                if (str[i] == '\0') break;
+                myStr += (char)str[i];
+            }
+            return myStr;
+        }
+        public static string ReadBodyNullTerminatedString(this Stream _bodyBuffer, int Offset, int MaxSize)
+        {
+            SetPosition(_bodyBuffer, Offset);
+            return ReadBodyNullTerminatedString(_bodyBuffer, MaxSize);
+        }
+
+        /// <summary>
+        /// Advances the <see cref="BodyPosition"/> by the <paramref name="amount"/> given.
+        /// </summary>
+        /// <param name="amount"></param>
+        public static void Advance(this Stream _bodyBuffer, int amount = 1) => SetPosition(_bodyBuffer, (int)_bodyBuffer.Position + amount);
+        /// <summary>
+        /// Reads all data until the end of the buffer
+        /// </summary>
+        /// <exception cref="NotImplementedException"></exception>
+        public static byte[] ReadToEnd(this Stream _bodyBuffer) => ReadBodyByteArray(_bodyBuffer, (int)(_bodyBuffer.Length - _bodyBuffer.Position));
+    }
+}
